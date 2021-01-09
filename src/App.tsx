5a431c343ea13b09d6faffe6e3907639ec91c6e8
smorgrav <torbjorn@smorgrav.org>
2021-01-09 20:19:07 +0100

    Add initial teaming

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,36 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import {FirebaseProvider} from './FirebaseProvider';
 import {CompetitionOverview} from './CompetitionOverivew';
+import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ab9f9f',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#f44336',
+    },
+  },
+});
 
 const App = () => {
   return (
-    <FirebaseProvider>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">News</Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <CompetitionOverview/>
-    </FirebaseProvider>
+    <ThemeProvider theme={theme}>
+      <FirebaseProvider>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">News</Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+        <CompetitionOverview/>
+      </FirebaseProvider>
+    </ThemeProvider>
   );
 };
 
