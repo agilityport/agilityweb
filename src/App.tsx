4dc5d70c9788113ec9a1eefe9913bf8973692db8
smorgrav <torbjorn@smorgrav.org>
2021-01-09 22:23:18 +0100

    Add tabbed menu to nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,7 +6,7 @@ import {FirebaseProvider} from './FirebaseProvider';
 import {CompetitionOverview} from './CompetitionOverivew';
 import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
-import {IconButton} from '@material-ui/core';
+import {IconButton, Tab, Tabs} from '@material-ui/core';
 import {AccountCircleOutlined} from '@material-ui/icons';
 
 const theme = createMuiTheme({
@@ -22,17 +22,33 @@ const theme = createMuiTheme({
 });
 
 const App = () => {
+  const [navItem, setNavItem] = useState(0);
+
   return (
     <ThemeProvider theme={theme}>
       <FirebaseProvider>
         <AppBar position="static">
           <Toolbar>
-            <Box display="flex" flexGrow={1}>
+            <Box display="flex" justifyContent="space-between"
+              flexGrow={1} alignItems="center">
               <img src="agilityportlogo.png" alt="logo" height="35px"/>
+              <Tabs
+                value={navItem}
+                onChange={(event, value)=>{
+                  setNavItem(value);
+                }}
+                indicatorColor="secondary"
+                textColor="inherit"
+                centered
+              >
+                <Tab label="Schedule" />
+                <Tab label="Results" />
+                <Tab label="Organize" />
+              </Tabs>
+              <IconButton color="inherit">
+                <AccountCircleOutlined/>
+              </IconButton>
             </Box>
-            <IconButton color="inherit">
-              <AccountCircleOutlined/>
-            </IconButton>
           </Toolbar>
         </AppBar>
         <CompetitionOverview/>
