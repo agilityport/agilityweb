69d6e89620c6dd2de5a9f09e090ebe7fdff97b6d
smorgrav <torbjorn@smorgrav.org>
2021-01-10 10:39:33 +0100

    Use react router and split schedule, results and organise into three paths

---
+++ src/organise/Organise.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Box} from '@material-ui/core';
+
+const Organise = () => {
+  return <Box>Page for organisers to run their events</Box>;
+};
+
+export {Organise};
